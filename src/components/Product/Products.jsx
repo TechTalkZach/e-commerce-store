b838506ba20f37aa29946fe8e2bc3f73dab96d4f
zach <zachalvarezxvi@gmail.com>
2022-10-07 23:32:38 -0400

    Creation of Commerce store by using commerce.js

--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product';
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://images.freeimages.com/images/premium/previews/4026/40269-url.jpg'},
@@ -11,8 +12,11 @@ const products = [
 
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
         <main>
+            <div className={classes.toolbar} />
         <Grid container justifyContent='center' spacing={4} >
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
