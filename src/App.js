1badd73de08aba7bf772b879f777cbe64c760a23
zach <zachalvarezxvi@gmail.com>
2022-10-08 12:38:04 -0400

    API key fully functional and calling products

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
  
 // import Products from './components/Product/Products';
@@ -8,6 +8,23 @@ import { Products, Navbar } from './components';
 
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    // Using destructured response from async products.list  to set data inside products and populate them
+    setProducts(data);
+  }
+
+  // Dependency Array set to empty so it only runs @ the start (Component did mount)
+  useEffect(() => {
+    fetchProducts();
+
+  }, []);
+
+  console.log(products);
+  
   return (
     <div>
        <Navbar />
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -2,4 +2,4 @@ import Commerce from "@chec/commerce.js";
 
 // API key from Commerce.js is hidden in .env 
 // **Creation of new commerce store**
-export const commerce = new Commerce(process.env(REACT_APP_CHEC_PUBLIC_KEY, true));
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
