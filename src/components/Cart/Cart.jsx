e39605cfaf86562fc09efa35b1b81ad7c3a7e289
zach <zachalvarezxvi@gmail.com>
2022-10-28 22:31:22 -0400

    Added props to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Container, Typography, Button, Grid  } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
 
-const Cart = ( { cart } ) => {
+const Cart = ( { cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart } ) => {
     // !=0 === !cart.line_items.lenth
     console.log(cart.line_items)
     // No need for variable here
@@ -13,7 +14,9 @@ const Cart = ( { cart } ) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-      <Typography variant='subtitle1'>You have no items in your shopping cart, start adding some!</Typography>
+      <Typography variant='subtitle1'>You have no items in your shopping cart
+        <Link to="/" className={classes.link}>start adding some items</Link>
+      </Typography>
     );
 
     const FilledCart = () => (
@@ -21,7 +24,7 @@ const Cart = ( { cart } ) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
           </Grid>
         ))}
       </Grid>
@@ -29,7 +32,7 @@ const Cart = ( { cart } ) => {
         <Typography variant="h4"> Subtotal : {cart.subtotal.formatted_with_symbol} </Typography>
         <div>
           <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
-          <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Fill Cart </Button>
+          <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Fill Cart </Button>
         </div>
 
       </div>
@@ -41,7 +44,7 @@ const Cart = ( { cart } ) => {
     return (
     <Container>
         <div className={classes.toolbar} />
-        <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+        <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
         { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
     </Container>
   )
