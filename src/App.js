e711f41e6bfc2f59c8a31e283e9ea57de7433594
zach <zachalvarezxvi@gmail.com>
2022-11-02 20:34:02 -0400

    Added cart prop to Checkout component

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ const App = () => {
                                         />}>
 
         </Route>
-        <Route path='/checkout' element={<Checkout />}></Route>
+        <Route path='/checkout' element={<Checkout cart={cart} />}></Route>
        </Routes>
     </div>
     </Router>
